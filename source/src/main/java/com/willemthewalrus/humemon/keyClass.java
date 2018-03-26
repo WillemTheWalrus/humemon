94684f9f5054aec446585af4aecc0d82b0cf90d1
WillemTheWalrus <billdoudna@gmail.com>
2018-03-26 15:11:17 -0700

    Delete keyClass.java

--- source/src/main/java/com/willemthewalrus/humemon/keyClass.java
+++
@@ -1,9 +0,0 @@
-package com.willemthewalrus.humemon;
-
-/**
- * Created by William on 3/24/2018.
- */
-
-public class keyClass {
-    public static final String key = "AIzaSyBC9ExL_zo1LKXYWSZxccj4mRaIJ7qJpQE";
-}
